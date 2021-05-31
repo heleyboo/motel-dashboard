1b9550b3d8eb23fa385c9d612425adfe75a8bb6a
Hoan Do Van <vanhoan90it6@gmail.com>
2021-05-31 10:17:26 +0700

    Hoan Update

--- src/main/java/com/binh/motel/controller/frontend/FrontCreateRoomController.java
+++ src/main/java/com/binh/motel/controller/frontend/FrontCreateRoomController.java
@@ -1,14 +1,16 @@
 package com.binh.motel.controller.frontend;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.binh.motel.dto.MotelRoomDto;
@@ -19,10 +21,10 @@ import com.binh.motel.service.ProvinceService;
 @Controller
 @RequestMapping("/dang-tin")
 public class FrontCreateRoomController {
-	
+
 	@Autowired
 	private ProvinceService provinceService;
-	
+
 	@GetMapping
 	public String showForm(Model model) {
 		MotelRoomDto motelRoomDto = new MotelRoomDto();
@@ -32,5 +34,21 @@ public class FrontCreateRoomController {
 		model.addAttribute("motelRoomDto", motelRoomDto);
 		model.addAttribute("directions", directions);
 		return "frontend/room/create";
+	}
+
+	@PostMapping
+	public String create(@Valid MotelRoomDto motelRoomDto, BindingResult bindingResult, Model model) {
+		if (bindingResult.hasErrors()) {
+			List<Province> provinces = provinceService.getAll();
+			List<RoomDirection> directions = Arrays.asList(RoomDirection.values());
+			model.addAttribute("provinces", provinces);
+			model.addAttribute("motelRoomDto", motelRoomDto);
+			model.addAttribute("directions", directions);
+			return "frontend/room/create";
+		}
+
+		// roomService.saveRoom()
+		return "redirect:/dang-tin";
+
 	}
 }
