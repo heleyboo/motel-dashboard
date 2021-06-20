173dc25ba4b4d58c65cae8bb05a3b6e5caf05aac
Thanh Binh <binh050598@gmail.com>
2021-06-20 11:15:51 +0700

    update messageTest

---
+++ src/main/java/com/binh/motel/controller/frontend/FrontMessagesTestController.java
@@ -0,0 +1,28 @@
+package com.binh.motel.controller.frontend;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+import com.binh.motel.entity.Message;
+
+@Controller
+public class FrontMessagesTestController {
+	
+	@MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public Message sendMessage(@Payload Message Message) {
+        return Message;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public Message addUser(@Payload Message Message,
+                               SimpMessageHeaderAccessor headerAccessor) {
+        // Add username in web socket session
+        headerAccessor.getSessionAttributes().put("username", Message.getMessageLines());
+        return Message;
+    }
+}
---
+++ src/main/java/com/binh/motel/controller/frontend/FrontWebSocketEventListenerController.java
@@ -0,0 +1,42 @@
+package com.binh.motel.controller.frontend;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import com.binh.motel.entity.Message;
+
+@Component
+public class FrontWebSocketEventListenerController {
+	private static final Logger logger = LoggerFactory.getLogger(FrontWebSocketEventListenerController.class);
+
+    @Autowired
+    private SimpMessageSendingOperations messagingTemplate;
+
+    @EventListener
+    public void handleWebSocketConnectListener(SessionConnectedEvent event) {
+        logger.info("Received a new web socket connection");
+    }
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        if(username != null) {
+            logger.info("User Disconnected : " + username);
+
+            Message messages = new Message();
+            messages.setType(Message.MessageType.LEAVE);
+            messages.setSender(username);
+
+            messagingTemplate.convertAndSend("/topic/public", messages);
+        }
+    }
+}
